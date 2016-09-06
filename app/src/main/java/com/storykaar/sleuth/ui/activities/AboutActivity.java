2b0709f87fe1107db9026040c97a2361f40b39d1
Pawan Hegde <h.pawan@gmail.com>
2016-09-06 22:19:12 +0530

    Settings Activity and About Activity get proper actionbars

--- app/src/main/java/com/storykaar/sleuth/ui/activities/AboutActivity.java
+++ app/src/main/java/com/storykaar/sleuth/ui/activities/AboutActivity.java
@@ -6,26 +6,18 @@ package com.storykaar.sleuth.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import com.storykaar.sleuth.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class AboutActivity extends AppCompatActivity {
-
-    @BindView(R.id.about_action_bar)
-    Toolbar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        ButterKnife.bind(this);
 
-        setSupportActionBar(actionBar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("About");
+        }
     }
-
-
 }
